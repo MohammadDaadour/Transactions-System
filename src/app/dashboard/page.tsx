3df92625a7d4b6656d0d3c1b6de95ff2090bac69
MohammadDaadour <daadourmohammad@gmail.com>
2026-06-12 11:40:36 +0300

    www

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -87,7 +87,7 @@ export default async function DashboardOverview() {
                                             </p>
                                         </div>
                                     );
-                                })}
+                                })} 
                         </div>
                     )}
                 </div> 
